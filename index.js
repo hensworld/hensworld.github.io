16998344eaa01852e7729d5bd343b755940c78b1
hensworld <165962638+hensworld@users.noreply.github.com>
2024-04-04 16:33:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var password="hey";
+var guess;
+<<<<<<< HEAD
+guess=prompt('hai!','');
+=======
+guess=prompt('hi','');
+>>>>>>> 1b69523fb0ec8fccd03f0a167c51f59f67ea71ee
+if(password==guess)
+alert('have a good time!');
+else
+{
+  window.location="http://www.google.com";
+}
