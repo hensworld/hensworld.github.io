020babc93a35d4b055512cb1a492f5f959afa976
hensworld <165962638+hensworld@users.noreply.github.com>
2024-04-23 16:43:49 +0000

    fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var password="redo";
+//login
+var password="reo";
 var guess;
 guess=prompt('try again :p','');
 if(password==guess)
@@ -6,4 +7,21 @@ alert('have a great day :)');
 else
 {
   window.location="https://www.youtube.com/watch?v=NHpczegqMUI";
+}
+//login
+
+
+function btnRed() {
+  document.getElementById("Div1").style.backgroundColor="Red";
+}
+function btnGreen() {
+  document.getElementById("Div1").style.backgroundColor="Green";
+}
+function btnBlue() {
+  document.getElementById("Div1").style.backgroundColor="Blue";
+}
+function btnReset() {
+  document.getElementById("Div1").style.backgroundColor="transparent";
+  document.getElementById("Div2").style.backgroundColor="transparent";
+  document.getElementById("Div3").style.backgroundColor="transparent";
 }
